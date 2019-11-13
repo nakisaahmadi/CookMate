b0b2f2486cd7e45b353009415e1201282e1fd4aa
Nakisa <nakisa.ahmadi@gmail.com>
2019-11-13 07:38:55 -0800

    test

--- CSE110_Firebase/Project110/src/edu/ucsd/cs110/tests/CelsiusTest.java
+++ CSE110_Firebase/Project110/src/edu/ucsd/cs110/tests/CelsiusTest.java
@@ -32,7 +32,7 @@ public class CelsiusTest extends TestCase{
         int endIndex = string.indexOf(ending);
 
         // (Hint: what is the length of the middle of the string?)
-        assertTrue(string.substring(0, endIndex).equals(beginning));
+        assertTrue(string.substring(1, endIndex).equals(beginning));
     }
 
     public void testCelsiusToCelsius()
